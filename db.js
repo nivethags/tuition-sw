55bfd4d39baf91331f3bd7d0a57d0adc3e68248a
nivethags <nivethagslakshmi@gmail.com>
2026-01-18 22:43:16 +0530

    Fix : MySQL Version Issue Solved

--- db.js
+++ db.js
@@ -5,17 +5,19 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
+  queueLimit: 0
 });
 
-
-db.connect((err) => {
+// Test connection
+db.query("SELECT 1", (err) => {
   if (err) {
-    console.error("❌ Error connecting to MySQL:", err.message);
-    return;
+    console.error("❌ MySQL connection failed:", err.message);
+  } else {
+    console.log("✅ Connected to Railway MySQL successfully!");
   }
-  console.log("✅ Connected to MySQL successfully!");
 });
 
 module.exports = db;
