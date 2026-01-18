b5ae4cae634939dbb7656d739e1f9a6b04071d16
nivethags <nivethagslakshmi@gmail.com>
2026-01-18 22:56:28 +0530

    Fix : DB MySQL Variable name updated

--- db.js
+++ db.js
@@ -1,20 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-// Test connection
 db.query("SELECT 1", (err) => {
   if (err) {
-    console.error("❌ MySQL connection failed:", err.message);
+    console.error("❌ MySQL connection failed:", err);
   } else {
     console.log("✅ Connected to Railway MySQL successfully!");
   }
