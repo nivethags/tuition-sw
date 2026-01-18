f6b75f54101ef0baa20a54d93c18263532046f40
nivethags <nivethagslakshmi@gmail.com>
2026-01-18 22:54:21 +0530

    Fix : DB connection issue Debugging.....

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.use(express.json());
 
 // Test route
 app.get("/", (req, res) => {
+  console.log("MYSQLHOST:", process.env.MYSQLHOST);
+console.log("MYSQLUSER:", process.env.MYSQLUSER);
+console.log("MYSQLDATABASE:", process.env.MYSQLDATABASE);
+console.log("MYSQLPORT:", process.env.MYSQLPORT);
+
   res.send("Welcome");
 });
 
